97038786b24e3d770ef5d817c8ec7e485023c050
Blaine Love <blainelove94@gmail.com>
2021-01-21 12:59:52 -0500

    commit tree 2e8c5e19fcedae22da648481e5aba8ff74b73996

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+   
+
+    def initialize
+        @knowledge = []
+     
+    end
+
+
+    def learn(k)
+        @knowledge << k
+    end
 
 end
